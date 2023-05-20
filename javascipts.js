1cf4a4b48cd53466ad419f163b6c67b9a1a4b4b6
lutricuong <tcuong.lu@gmail.com>
2023-05-20 17:21:10 +0700

    add 16x16 grid

---
+++ javascipts.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container');
+const arrayGrid = [];
+for (let i=1;i<=16;i++) {
+    arrayGrid[i] = document.createElement('div');
+    container.appendChild(arrayGrid[i]);
+    arrayGrid[i].classList.add('rowGrid');
+    for (let j=1;j<=16;j++) {
+        arrayGrid[i][j] = document.createElement('div');
+        arrayGrid[i].appendChild(arrayGrid[i][j]);
+        arrayGrid[i][j].classList.add('singleGrid');
+    }
+}
+
