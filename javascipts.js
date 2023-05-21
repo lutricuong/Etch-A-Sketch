1eb97f37fe7329151613409ba948c0cd418e1319
lutricuong <tcuong.lu@gmail.com>
2023-05-21 15:23:46 +0700

    add fixed total space 320px

--- javascipts.js
+++ javascipts.js
@@ -1,10 +1,12 @@
 const container = document.querySelector('.container');
 const arrayGrid = [];
-for (let i=1;i<=16;i++) {
+let column = 16;
+let row = 16;
+for (let i=1;i<=column;i++) {
     arrayGrid[i] = document.createElement('div');
     container.appendChild(arrayGrid[i]);
-    arrayGrid[i].classList.add('rowGrid');
-    for (let j=1;j<=16;j++) {
+    arrayGrid[i].classList.add('columnGrid');
+    for (let j=1;j<=row;j++) {
         arrayGrid[i][j] = document.createElement('div');
         arrayGrid[i].appendChild(arrayGrid[i][j]);
         arrayGrid[i][j].classList.add('singleGrid');
